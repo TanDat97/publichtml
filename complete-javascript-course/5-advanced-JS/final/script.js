96d2c8419436fc8576887a1c4f2add191627437b
Tan Dat <dat.tran@tripath.vn>
2020-09-07 13:45:19 +0700

    test

--- complete-javascript-course/5-advanced-JS/final/script.js
+++ complete-javascript-course/5-advanced-JS/final/script.js
@@ -329,54 +329,54 @@ c) correct answer (I would use a number for this)
 */
 
 
-/*
-(function() {
-    function Question(question, answers, correct) {
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
 
-    Question.prototype.displayQuestion = function() {
-        console.log(this.question);
+// (function() {
+//     function Question(question, answers, correct) {
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
 
-        for (var i = 0; i < this.answers.length; i++) {
-            console.log(i + ': ' + this.answers[i]);
-        }
-    }
+//     Question.prototype.displayQuestion = function() {
+//         console.log(this.question);
 
-    Question.prototype.checkAnswer = function(ans) {
-        if (ans === this.correct) {
-            console.log('Correct answer!');
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ': ' + this.answers[i]);
+//         }
+//     }
 
-        } else {
-            console.log('Wrong answer. Try again :)')
-        }
-    }
+//     Question.prototype.checkAnswer = function(ans) {
+//         if (ans === this.correct) {
+//             console.log('Correct answer!');
 
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
-                          ['Yes', 'No'],
-                          0);
+//         } else {
+//             console.log('Wrong answer. Try again :)')
+//         }
+//     }
 
-    var q2 = new Question('What is the name of this course\'s teacher?',
-                          ['John', 'Micheal', 'Jonas'],
-                          2);
+//     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+//                           ['Yes', 'No'],
+//                           0);
 
-    var q3 = new Question('What does best describe coding?',
-                          ['Boring', 'Hard', 'Fun', 'Tediuos'],
-                          2);
+//     var q2 = new Question('What is the name of this course\'s teacher?',
+//                           ['John', 'Micheal', 'Jonas'],
+//                           2);
 
-    var questions = [q1, q2, q3];
+//     var q3 = new Question('What does best describe coding?',
+//                           ['Boring', 'Hard', 'Fun', 'Tediuos'],
+//                           2);
 
-    var n = Math.floor(Math.random() * questions.length);
+//     var questions = [q1, q2, q3];
 
-    questions[n].displayQuestion();
+//     var n = Math.floor(Math.random() * questions.length);
 
-    var answer = parseInt(prompt('Please select the correct answer.'));
+//     questions[n].displayQuestion();
 
-    questions[n].checkAnswer(answer);
-})();
-*/
+//     var answer = parseInt(prompt('Please select the correct answer.'));
+
+//     questions[n].checkAnswer(answer);
+// })();
+
 
 
 
@@ -393,7 +393,7 @@ c) correct answer (I would use a number for this)
 */
 
 
-/*
+
 (function() {
     function Question(question, answers, correct) {
         this.question = question;
@@ -472,4 +472,3 @@ c) correct answer (I would use a number for this)
     nextQuestion();
     
 })();
-*/
