ca4766de522b32cc97c2f9fdd7d9f267cb403d33
Tran Tan Dat <trantandat130497@gmail.com>
2020-09-20 15:08:30 +0700

    car

---
+++ advanced-css-course/natours-starter/script.js
@@ -0,0 +1,63 @@
+let car = [
+  {
+    time: 0,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 10,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 20,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 30,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 40,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 50,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 60,
+    bank: 1,
+    cross: false,
+  },
+  {
+    time: 70,
+    bank: 1,
+    cross: false,
+  },
+];
+
+const max = 2;
+const time  = 10;
+
+let ship = {
+  bank: 1,
+  time: 0,
+  slot: 0,
+}
+
+let i = 0;
+
+while(true) {
+  if (car[i].time <= ship.time && car[i].bank === ship.bank && !cross) {
+    slot += 1;
+  } else {
+
+    ship.time += time
+  }
+  
+}
